2fff1ad8918ee9fd4143b36697b1c50e1c8f0e8a
Agith-Ravn <76996657+Agith-Ravn@users.noreply.github.com>
2021-07-24 13:03:21 +0200

    Repetisjon

---
+++ M3/Oppgave19.1/Oppgave19.1/Match.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Oppgave19._1
+{
+    public class Match
+    {
+        //Husk! Private = kun nås fra samme class og public fra alle class.
+        //get og set bestemmer bare hvem som kan hente og endre.
+        private string bet { get; set; }
+        public bool IsRunning { get; private set; }
+        public int homeGoals { get; private set; }
+        public int awayGoals { get; private set; }
+        
+        public Match(string bet)
+        {
+            this.bet = bet;
+            IsRunning = true;
+
+        }
+
+        public void Stop()
+        {
+            IsRunning = false;
+        }
+
+        public void AddGoal(string command)
+        {
+            if (command == "H") homeGoals++;
+            else if (command == "B") awayGoals++;
+        }
+
+        public bool IsBetCorrect()
+        {
+            var result = homeGoals == awayGoals ? "U" : homeGoals > awayGoals ? "H" : "B";
+            return bet.Contains(result);
+        }
+
+        public string GetScore()
+        {
+            return $"{homeGoals}-{awayGoals}";
+        }
+    }
+}
---
+++ M3/Oppgave19.1/Oppgave19.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Oppgave19._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Gyldig tips: \r\n - H, U, B\r\n - halvgardering: HU, HB, UB\r\n - helgardering: HUB\r\nHva har du tippet for denne kampen? ");
+            var bet = Console.ReadLine();
+            var match = new Match(bet);
+
+            while (match.IsRunning)
+            {
+                Console.Write("Kommandoer: \r\n - H = scoring hjemmelag\r\n - B = scoring bortelag\r\n - X = kampen er ferdig\r\nAngi kommando: ");
+                var command = Console.ReadLine();
+                if (command == "X") match.Stop();
+                if (command == "H" || command == "B") match.AddGoal(command);
+                Console.WriteLine($"Stillingen er {match.GetScore()}");
+            }
+            var isBetCorrectText = match.IsBetCorrect() ? "riktig" : "feil";
+            Console.WriteLine($"Du tippet {isBetCorrectText}");
+        }
+    }
+}
