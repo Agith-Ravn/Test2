cdf221db5a7d448fea966ddfc9c19f94c8019b7c
Agith-Ravn <76996657+Agith-Ravn@users.noreply.github.com>
2021-08-20 20:41:11 +0200

    Øvelse3 (Uferdig)

---
+++ GET IT/Øvelse3/Øvelse3/Data/Migrations/20210820181135_initialsetup.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Øvelse3.Data.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Joke",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    JokeQuestion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MyProperty = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Joke", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Joke");
+        }
+    }
+}
---
+++ GET IT/Øvelse3/Øvelse3/Models/Joke.cs
@@ -0,0 +1,14 @@
+﻿namespace Øvelse3.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string MyProperty { get; set; }
+
+        public Joke()
+        {
+            
+        }
+    }
+}
