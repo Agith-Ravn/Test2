2bf9cd6cb5f6326bc19136c791bb2a3c81fccb07
Agith-Ravn <76996657+Agith-Ravn@users.noreply.github.com>
2021-08-29 23:31:16 +0200

    .

---
+++ GET IT/Øvelse4/Øvelse4/Controllers/PersonController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Øvelse4.Model;
+
+namespace Øvelse4.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public Person GetOne(int id)
+        {
+            return new Person()
+            {
+                Id = id,
+                Email = "test@epost.net",
+                Name = "Test"
+            };
+        }
+
+        public IEnumerable<Person> GetMany()
+        {
+
+        }
+    }
+}
---
+++ GET IT/Øvelse4/Øvelse4/Model/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace Øvelse4.Model
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+    }
+}
